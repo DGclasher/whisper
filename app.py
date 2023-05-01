bf6d51e51d02abe81d6ca166cf425834b114abbf
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-05-02 03:01:28 +0530

    improvised

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import db
 import secrets
 from dotenv import load_dotenv
+from bson.json_util import dumps
+from datetime import datetime
 from pymongo.errors import DuplicateKeyError
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_socketio import SocketIO, join_room, leave_room
@@ -30,6 +32,10 @@ def home():
 def join():
     return render_template('join.html')
 
+@app.route('/myinfo')
+@login_required
+def myinfo():
+    return render_template('myinfo.html', current_user=current_user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -66,6 +72,12 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html')
 
+@app.route('/delete', methods=['POST'])
+@login_required
+def delete_me():
+    db.delete_user(current_user.username)
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/logout')
 @login_required
@@ -84,11 +96,15 @@ def create_room():
                      for username in request.form.get('members').split(',')]
         if len(room_name) and len(usernames):
             room_id = db.save_room(room_name, current_user.username)
-            if current_user.username in usernames:
-                usernames.remove(current_user.username)
-            db.add_room_members(room_id, room_name,
-                                usernames, current_user.username)
-            return redirect(url_for('home'))
+            if room_id:
+                if current_user.username in usernames:
+                    usernames.remove(current_user.username)
+                db.add_room_members(room_id, room_name,
+                                    usernames, current_user.username)
+                return redirect(url_for('home'))
+            else:
+                message = 'Room with same name exists'
+                return render_template('create_room.html', message=message)
         else:
             message = 'Failed to create room'
             return render_template('create_room.html', message=message)
@@ -112,7 +128,7 @@ def del_room(room_name):
 def edit_room(room_name):
     try:
         room = db.get_room(room_name)
-        if room and db.is_room_admin(str(room['_id']), current_user.username):
+        if room and db.is_room_admin(room['_id'], current_user.username):
             current_members = [member['_id']['username']
                                for member in db.get_room_members(room['_id'])]
             if request.method == 'POST':
@@ -123,43 +139,56 @@ def edit_room(room_name):
                 members_to_add = list(set(new_members) - set(current_members))
                 members_to_delete = list(set(current_members)-set(new_members))
                 if len(members_to_add):
-                    db.add_room_members(str(room['_id']), room['room_name'], members_to_add, current_user.username)
+                    db.add_room_members(
+                        room['_id'], room['room_name'], members_to_add, current_user.username)
                 if len(members_to_delete):
-                    db.remove_room_members(str(room['_id']), members_to_delete)
+                    db.remove_room_members(room['_id'], members_to_delete)
+                return redirect(url_for('chat', room_name=room_name))
             current_members_str = ','.join(current_members)
             return render_template('edit_room.html', room=room, current_user=current_user, current_members_str=current_members_str)
-        return redirect(request.referrer)
+        return redirect(url_for('home'))
     except:
-        return redirect(request.referrer)
+        return redirect(url_for('home'))
 
 
 @app.route('/chat/<room_name>/', methods=['GET', 'POST'])
+@login_required
 def chat(room_name):
-    try:
-        room = db.get_room(room_name)
-        if room and db.is_room_member(str(room['_id']), current_user.username):
-            room_members = db.get_room_members(str(room['_id']))
-            print(room_members)
-            return render_template('chat.html', room=room, current_user=current_user, room_members=room_members)
-    except:
+    room = db.get_room(room_name)
+    if room and db.is_room_member(room['_id'], current_user.username):
+        room_members = db.get_room_members(room['_id'])
+        messages = db.get_messages(str(room['_id']))
+        return render_template('chat.html', room=room, current_user=current_user, room_members=room_members, messages=messages)
+    else:
         message = "Unable to join that room"
         return render_template('join.html', message=message)
-    return redirect(url_for('home'))
+
+
+@app.route('/chat/<room_name>/messages/')
+@login_required
+def get_older_messages(room_name):
+    room = db.get_room(room_name)
+    if room and db.is_room_member(room['_id'], current_user.username):
+        page = int(request.args.get('page', 0))
+        room_members = db.get_room_members(room['_id'])
+        messages = db.get_messages(str(room['_id']), page)
+        return dumps(messages)
+    else:
+        message = "Unable to join that room"
+        return render_template('join.html', message=message)
 
 
 @socketio.on('join')
 def on_join(data):
-    username = data['username']
     room = data['room']
     join_room(room)
     socketio.emit('join_room', data, room=room)
-    app.logger.info("{} has joined {}".format(username, room))
 
 
 @socketio.on('send_message')
 def on_send_message(data):
-    app.logger.info("{} in room {} has sent message {}".format(
-        data['username'], data['room'], data['message']))
+    data['sent_at'] = datetime.now().strftime("%d/%b:%H:%M")
+    db.save_message(data['room'], data['message'], data['username'])
     socketio.emit('receive_message', data, room=data['room'])
 
 
--- tasks.py
+++ tasks.py
@@ -1,6 +1,5 @@
 import os
 import db
-import time
 import pytz
 import threading
 from dotenv import load_dotenv
@@ -15,7 +14,7 @@ user_collections = db_remote.get_collection("user")
 
 
 def delete_old_users():
-    cutoff_time = datetime.utcnow() - timedelta(hours=24)
+    cutoff_time = datetime.now() - timedelta(hours=24)
     cutoff_time = cutoff_time.replace(tzinfo=pytz.UTC)
     old_users = list(user_collections.find(
         {'created_at': {'$lt': cutoff_time}}))
@@ -24,11 +23,8 @@ def delete_old_users():
             db.delete_user(user['_id'])
 
 
-def backgroud_task_1():
-    while True:
-        delete_old_users()
-        time.sleep(86400)
-
-
-threading.Thread(target=backgroud_task_1).start()
-print("task running")
+start_time = datetime.now()
+print("Task Starting",)
+delete_old_users()
+end_time = datetime.now()
+print("Task Ended\nTime Taken:", (end_time-start_time).seconds)
